4b8360ed884812f0e1007436b401511e3ce72b97
photowey <photowey@gmail.com>
2022-07-16 23:01:57 +0800

    feat: 1.Add regexpx package

---
+++ pkg/Regexpx/regexpx_test.go
@@ -0,0 +1,54 @@
+package Regexpx
+
+import (
+	"testing"
+)
+
+func TestRegexpExtract(t *testing.T) {
+	type args struct {
+		regex string
+		src   string
+		temp  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Test extract annotation name",
+			args: args{
+				regex: `^@(?P<annotation>[\S]+)\(.*\)`,
+				src:   "@Service(\"helloService\")",
+				temp:  "$annotation",
+			},
+			want: "Service",
+		},
+		{
+			name: "Test extract annotation string value",
+			args: args{
+				regex: `^@.*\((?P<value>[\S]+)\)`,
+				src:   "@Service(\"helloService\")",
+				temp:  "$value",
+			},
+			want: "\"helloService\"",
+		},
+		{
+			name: "Test extract annotation json value",
+			args: args{
+				regex: `^@.*\((?P<value>[\S]+)\)`,
+				src:   "@ComponentScan({\"path\":\"github.com/photowey/parsergo/tests\",\"excludes\":[\"github.com/photowey/parsergo/tests/structx\"]})",
+				temp:  "$value",
+			},
+			want: "{\"path\":\"github.com/photowey/parsergo/tests\",\"excludes\":[\"github.com/photowey/parsergo/tests/structx\"]}",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := RegexpExtract(tt.args.regex, tt.args.src, tt.args.temp)
+			if got != tt.want {
+				t.Errorf("RegexpExtract() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
